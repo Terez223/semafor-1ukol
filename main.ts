2bf77fd2b0a323ebde77a04f979c94ce26aec6ed
Terez223 <114877792+Terez223@users.noreply.github.com>
2022-10-02 20:59:32 +0200

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,14 @@
 function Green_LED_lights_on () {
+    strip.setPixelColor(20, neopixel.colors(NeoPixelColors.Green))
+    strip.setPixelColor(19, neopixel.colors(NeoPixelColors.Green))
+    strip.setPixelColor(18, neopixel.colors(NeoPixelColors.Green))
+    strip.setPixelColor(17, neopixel.colors(NeoPixelColors.Green))
+    strip.setPixelColor(16, neopixel.colors(NeoPixelColors.Green))
+    strip.setPixelColor(15, neopixel.colors(NeoPixelColors.Green))
+    strip.setPixelColor(14, neopixel.colors(NeoPixelColors.Green))
+    strip.setPixelColor(13, neopixel.colors(NeoPixelColors.Green))
+    strip.setPixelColor(12, neopixel.colors(NeoPixelColors.Green))
+    strip.setPixelColor(11, neopixel.colors(NeoPixelColors.Green))
     strip.setPixelColor(10, neopixel.colors(NeoPixelColors.Green))
     strip.setPixelColor(9, neopixel.colors(NeoPixelColors.Green))
     strip.setPixelColor(8, neopixel.colors(NeoPixelColors.Green))
@@ -13,6 +23,16 @@ function Green_LED_lights_on () {
     strip.show()
 }
 function Yellow_LED_lights_on () {
+    strip.setPixelColor(20, neopixel.colors(NeoPixelColors.Yellow))
+    strip.setPixelColor(19, neopixel.colors(NeoPixelColors.Yellow))
+    strip.setPixelColor(18, neopixel.colors(NeoPixelColors.Yellow))
+    strip.setPixelColor(17, neopixel.colors(NeoPixelColors.Yellow))
+    strip.setPixelColor(16, neopixel.colors(NeoPixelColors.Yellow))
+    strip.setPixelColor(15, neopixel.colors(NeoPixelColors.Yellow))
+    strip.setPixelColor(14, neopixel.colors(NeoPixelColors.Yellow))
+    strip.setPixelColor(13, neopixel.colors(NeoPixelColors.Yellow))
+    strip.setPixelColor(12, neopixel.colors(NeoPixelColors.Yellow))
+    strip.setPixelColor(11, neopixel.colors(NeoPixelColors.Yellow))
     strip.setPixelColor(10, neopixel.colors(NeoPixelColors.Yellow))
     strip.setPixelColor(9, neopixel.colors(NeoPixelColors.Yellow))
     strip.setPixelColor(8, neopixel.colors(NeoPixelColors.Yellow))
@@ -27,6 +47,16 @@ function Yellow_LED_lights_on () {
     strip.show()
 }
 function Red_LED_lights_on () {
+    strip.setPixelColor(20, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(19, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(18, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(17, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(16, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(15, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(14, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(13, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(12, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(11, neopixel.colors(NeoPixelColors.Red))
     strip.setPixelColor(10, neopixel.colors(NeoPixelColors.Red))
     strip.setPixelColor(9, neopixel.colors(NeoPixelColors.Red))
     strip.setPixelColor(8, neopixel.colors(NeoPixelColors.Red))
@@ -41,7 +71,7 @@ function Red_LED_lights_on () {
     strip.show()
 }
 let strip: neopixel.Strip = null
-strip = neopixel.create(DigitalPin.P0, 10, NeoPixelMode.RGB)
+strip = neopixel.create(DigitalPin.P0, 20, NeoPixelMode.RGB)
 basic.forever(function () {
     Red_LED_lights_on()
     basic.pause(3000)
